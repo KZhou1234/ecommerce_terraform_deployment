e9fc28b5c56964e13b3fee80c539b09a08fb7411
Ubuntu <ubuntu@ip-172-31-44-199.ec2.internal>
2024-10-30 01:14:47 +0000

    User data Scripts

---
+++ Scripts/backend_setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+git clone https://github.com/KZhou1234/ecommerce_terraform_deployment.git
+
+python3.9 -m venv venv
+source venv/bin/activate
+cd ecommerce_terraform_deployment/backend
+pip install -r requirements.txt
+
+# add the private ip
+#sed -i 's/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \["your_ip_address"\]/' settings.py
+sed -i "s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\"${backend_ip}\"\]/" settings.py
+
+
+# run the Django application
+python manage.py runserver 0.0.0.0:8000
---
+++ Scripts/frontend_setup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/KZhou1234/ecommerce_terraform_deployment.git
+curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
+sudo apt install -y nodejs
+
+
+#sed -i 's/http:\/\/private_ec2_ip:8000/http:\/\/${backend_ip}:8000/' package.json
+sed -i "s/http:\/\/private_ec2_ip:8000/http:\/\/${backend_ip}:8000/" package.json
+
+npm i
+export NODE_OPTIONS=--openssl-legacy-provider
+npm start
